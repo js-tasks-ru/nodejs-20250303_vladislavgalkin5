5915911a78daed6878e9aa4944165e5a3ece4ace
Vladislav Galkin <vladislavgalkin5@gmail.com>
2025-03-10 23:37:25 +0200

    Revert "Added task Сумма двух чисел"
    This reverts commit 0746083f99efdaf9b9cbee01924973e3613a1272.

--- 01-intro/01-sum/sum.js
+++ 01-intro/01-sum/sum.js
@@ -1,6 +1,6 @@
 export default function sum(a, b) {
-    if (typeof a !== 'number' || typeof b !== 'number') {
-      throw new TypeError('Оба аргумента должны быть числами');
-    }
-    return a + b;
+  if (typeof a !== 'number' || typeof b !== 'number') {
+    throw new TypeError('Оба аргумента должны быть числами');
   }
+  return a + b;
+}
