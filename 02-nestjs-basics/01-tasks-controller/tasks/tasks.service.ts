8bfd3aef3ff8e473c1260ee9864c5c0d191f9238
Vladislav Galkin <vladislavgalkin5@gmail.com>
2025-03-16 19:57:17 +0200

    POST / GET created

--- 02-nestjs-basics/01-tasks-controller/tasks/tasks.service.ts
+++ 02-nestjs-basics/01-tasks-controller/tasks/tasks.service.ts
@@ -1,17 +1,42 @@
 import { Injectable, NotFoundException } from "@nestjs/common";
-import { Task } from "./task.model";
+import { Task, TaskStatus } from "./task.model";
 
 @Injectable()
 export class TasksService {
   private tasks: Task[] = [];
 
-  getAllTasks(): Task[] {}
+  getAllTasks(): Task[] {
+    return this.tasks
+  }
 
-  getTaskById(id: string): Task {}
+  // getTaskById(id: string): Task {}
 
-  createTask(task: Task): Task {}
+  createTask(task: Task): Task {
 
-  updateTask(id: string, update: Task): Task {}
+    if (Object.values(TaskStatus).includes(task.status)){
 
-  deleteTask(id: string): Task {}
+      this.tasks.push({
+        id: (this.tasks.length + 1).toString(), 
+        ...task
+      }) 
+
+    } else {
+
+      throw new TypeError (`The status should be one of the following: ${Object.values(TaskStatus)}`);
+
+    }
+
+    return this.tasks.at(-1)
+    
+  }
+
+  // updateTask(id: string, update: Task): Task {}
+
+//   deleteTask(id: string): Task {
+//     let task = this.tasks.find(task => task.id == id)
+
+//     this.tasks.splice(this.tasks.indexOf(task), 1)
+
+//     return this.tasks
+//   }
 }
