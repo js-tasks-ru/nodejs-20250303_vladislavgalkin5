7c718943d26abf18b0882cb8e7bd39eafc9dd894
Vladislav Galkin <vladislavgalkin5@gmail.com>
2025-03-31 00:56:09 +0300

    Pipe implementation

--- 04-request-lifecycle/01-nestjs-components/tasks/tasks.controller.ts
+++ 04-request-lifecycle/01-nestjs-components/tasks/tasks.controller.ts
@@ -4,12 +4,12 @@ import {
   Delete,
   Get,
   Param,
-  ParseIntPipe,
   Patch,
   Post,
 } from "@nestjs/common";
 import { TasksService } from "./tasks.service";
 import { CreateTaskDto, UpdateTaskDto } from "./task.model";
+import { ParseIntPipe } from "pipes/parse-int.pipe";
 
 @Controller("tasks")
 export class TasksController {
