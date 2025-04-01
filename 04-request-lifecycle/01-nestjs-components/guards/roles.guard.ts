35febee67a3005abe64eb8a298f8b852abecb44e
Vladislav Galkin <vladislavgalkin5@gmail.com>
2025-04-01 23:59:46 +0300

    JWT decode implemented

--- 04-request-lifecycle/01-nestjs-components/guards/roles.guard.ts
+++ 04-request-lifecycle/01-nestjs-components/guards/roles.guard.ts
@@ -1,12 +1,19 @@
 import { CanActivate, ExecutionContext, ForbiddenException } from "@nestjs/common";
+import { jwtDecode } from 'jwt-decode';
+
 
 export class RolesGuard implements CanActivate {
   canActivate(context: ExecutionContext) {
     
     const ctx = context.switchToHttp();
-    const req = ctx.getRequest()
+    const req = ctx.getRequest();
+
+    if (req.headers.authorization) {
+      const role = jwtDecode(req.headers.authorization)['x-role']
+      console.log(role)
+    }
     
-    if (req.headers['x-role'] === 'admin') {
+    if (req.headers['x-role'] === 'admin' ) {
       return true
     } else {
       throw new ForbiddenException(`Доступ запрещён: требуется роль admin`)
