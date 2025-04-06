7b641e562dbf57b47a2eb95fa29a16c28f6289d3
Vladislav Galkin <vladislavgalkin5@gmail.com>
2025-04-06 21:00:03 +0300

    fix database path

--- 05-db/01-crud-tasks/config/database.ts
+++ 05-db/01-crud-tasks/config/database.ts
@@ -1,10 +1,11 @@
 import { registerAs } from '@nestjs/config'
 import { TypeOrmModuleOptions } from '@nestjs/typeorm'
+import * as path from 'path'
 
 export default registerAs('database', () => {
     return {
         type: 'sqlite',
-        database: './db.sqlite',
+        database: path.join(__dirname, '..', 'db.sqlite'),
         autoLoadEntities: true
     } as TypeOrmModuleOptions
 })
